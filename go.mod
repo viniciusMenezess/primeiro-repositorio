543eb5aa3248a34eadafd05214c1e16a09dab000
Vinicius Menezes de Oliveira <vinicius.menezes@ni-1898.skyonecorp.local>
2026-05-31 13:50:01 -0300

    :rocket: Iniciando repositório

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fabricioveronez/github-flow
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Desenvolvimento")
+	fmt.Println("Feature-XPTO")
+	fmt.Println("Hotfix-XPTO")
+}
